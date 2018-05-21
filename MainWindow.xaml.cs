c53b01eaead67a3473ad3c5dc8ed18075ea90354
Michael Coupland <couplandihc@gmail.com>
2018-05-21 15:18:43 -0500

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+
+namespace extensions_and_imagesource
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            testimage.FromFile(ConfigurationManager.AppSettings["imagepath"].ToString());            
+        }
+    }
+    public static class ImageExtensions
+    {
+        public static void FromFile(this Image image, string filename)
+        {
+            BitmapImage bmp = new BitmapImage();
+            bmp.BeginInit();
+            bmp.UriSource = new Uri(filename);
+            bmp.EndInit();
+            image.Source = bmp;
+        }
+    }
+}
